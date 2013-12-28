ab08a34961dc8d04f6160b10279f9cffe7debaad
terminator <andrewaka39@gmail.com>
2013-12-28 16:06:34 +0200

    1.2 Models

--- config/routes.rb
+++ config/routes.rb
@@ -1,13 +1,10 @@
 Social::Application.routes.draw do
 
+  resources :benefits
+  resources :profiles
+
   match '/how_this_works' => "main#how_this_works", via: :get
   root 'main#index'
-
-  # The priority is based upon order of creation: first created -> highest priority.
-  # See how all your routes lay out with "rake routes".
-
-  # You can have the root of your site routed with "root"
-  # root 'welcome#index'
 
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
