b2a1fed3dd3f4534f8e7f29d35c5fa6e14a349cc
terminator <andrewaka39@gmail.com>
2013-12-28 16:10:11 +0200

    1.2 Models

---
+++ app/controllers/benefits_controller.rb
@@ -0,0 +1,19 @@
+class BenefitsController < ApplicationController
+  def index
+  end
+
+  def show
+  end
+
+  def edit
+  end
+
+  def delete
+  end
+
+  def update
+  end
+
+  def create
+  end
+end
---
+++ app/controllers/profiles_controller.rb
@@ -0,0 +1,19 @@
+class ProfilesController < ApplicationController
+  def index
+  end
+
+  def show
+  end
+
+  def edit
+  end
+
+  def delete
+  end
+
+  def create
+  end
+
+  def update
+  end
+end
